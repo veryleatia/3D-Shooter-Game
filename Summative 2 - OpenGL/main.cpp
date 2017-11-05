a669fc3d932073db99ebc320e692016af6ffadfa
Aimee Constable <Veryleatia@gmail.com>
2017-11-05 18:56:49 +1300

    broken but clean ughhh

--- Summative 2 - OpenGL/main.cpp
+++ Summative 2 - OpenGL/main.cpp
@@ -1,7 +1,7 @@
 #include "Dependencies\glew\glew.h"
 #include "Dependencies\freeglut\freeglut.h"
 #include "Dependencies\soil\SOIL.h"
-#include "Dependencies\freetype2\freetype\freetype.h"
+//#include "Dependencies\freetype2\freetype\freetype.h"
 #include "glm\glm.hpp"
 #include "glm\gtc\matrix_transform.hpp"
 #include "glm\gtc\type_ptr.hpp"
@@ -10,4 +10,117 @@
 
 #include <ctime>
 
-cSceneManager* scenes = new cSceneManager();
+cSceneManager* game = new cSceneManager();
+
+GLfloat prevTime = 0;
+
+FMOD::System* audioMgr;
+FMOD::Sound* bgMusic;
+FMOD::Sound* hitSound;
+FMOD::Channel* channel;
+FMOD::Channel* channel2;
+
+//TextLabel* label;
+
+bool InitFmod() {
+	FMOD_RESULT result;
+
+	result = FMOD::System_Create(&audioMgr);
+	if (result != FMOD_OK) return false;
+
+	result = audioMgr->init(50, FMOD_INIT_NORMAL, 0);
+	if (result != FMOD_OK) return false;
+
+	return true;
+}
+
+const bool LoadAudio() {
+	FMOD_RESULT result;
+
+	result = audioMgr->createSound("assets/audio/hitsound.mp3", FMOD_DEFAULT, 0, &hitSound);
+	result = audioMgr->createSound("assets/audio/bgMusic.mp3", FMOD_DEFAULT, 0, &bgMusic);
+
+	bgMusic->setMode(FMOD_LOOP_NORMAL);
+
+	return true;
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	game->keyboard(key);
+}
+
+void keyboard_up(unsigned char key, int x, int y)
+{
+	game->keyboard_up(key);
+}
+
+void init()
+{
+
+	game->init();
+
+	InitFmod();
+	LoadAudio();
+	audioMgr->playSound(bgMusic, 0, false, &channel);
+
+
+}
+
+void render()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	game->render();
+
+	//label->Render();
+
+	glutSwapBuffers();
+}
+
+void update()
+{
+	GLfloat currentTime = glutGet(GLUT_ELAPSED_TIME);
+
+	GLfloat deltaTime = (currentTime - prevTime) * 0.08f;
+	prevTime = currentTime;
+
+	game->update(deltaTime);
+
+	if (game->getIsDeleted())
+	{
+		delete game;
+		game = new cSceneManager();
+		game->init();
+	}
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(560, 240);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Hamish's Graphics Game");
+
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	glewInit();
+	init();
+
+
+	glEnable(GL_DEPTH_TEST);
+
+
+	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(keyboard_up);
+
+	// register callbacks
+	glutDisplayFunc(render);
+	glutIdleFunc(update);
+	glutMainLoop();
+
+	return(0);
+}
--- Summative 2 - OpenGL/sceneManager.h
+++ Summative 2 - OpenGL/sceneManager.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "Dependencies\glew\glew.h"
 #include "Dependencies\freeglut\freeglut.h"
 #include "Dependencies\soil\SOIL.h"
@@ -14,41 +15,23 @@
 #include "Powerup.h"
 
 #define BUTTON_UP 0
-#define BUTTON_DOWN 1;
+#define BUTTON_DOWN 1
+unsigned char keyState[255];
 
 class cSceneManager
 {
-private:
-	unsigned char keyState[255];
-	bool playerAlive;
-	bool playerWin;
-	bool playerLose;
-	bool enemy3MoveUp;
-	bool isDeleted;
-	int numOfEnemies;
-	float cooldown;
-	int score;
-	int lives;
-	int level;
-	int rendernumber;
-	float playerSpeed;
-
-	cEnemies* enemy;
-	cPlayer* player;
-	cSprite* gameover;
-	cSprite* mainmenu;
-	cSprite* background;
-	cSprite* win;
-	cSprite* lose;
-	cCamera* camera;
-	std::vector<cPowerup*> VectorPowerups;
-	std::vector<cBullet*> VectorBullets;
-	std::vector<cEnemies*> VectorEnemies;
-
 public:
+	cSceneManager()
+	{
+	}
+
 	void init()
 	{
-		//change to a level system
+		std::cout << "WASD To move the character" << std::endl;
+		std::cout << "jilk to make the player shoot" << std::endl;
+		std::cout << "The player box is blue" << std::endl;
+		std::cout << "Red blocks are enemies, pink are powerups" << std::endl;
+		std::cout << "Walk over the powerup to pick it up, it will grant a small speed boost" << std::endl;
 
 		background->setNewPosition(glm::vec3(-1.0f, 0.0f, 0.0f));
 		background->setScale(glm::vec3(15.0f, 30.0f, 1.0f));
@@ -96,20 +79,363 @@ public:
 		lose->init();
 	}
 
-	bool getIsDeleted()
-	{
-		return isDeleted;
-	}
-
 	void render()
 	{
 		if (rendernumber == 0)
 		{
 			mainmenu->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+
+		}
+		else if (rendernumber == 1)
+		{
+			background->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+
+			if (!VectorBullets.empty())
+			{
+				for (int i = 0; i < VectorBullets.size(); ++i)
+				{
+					VectorBullets[i]->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+				}
+			}
+			if (!VectorEnemies.empty())
+			{
+				for (int i = 0; i < VectorEnemies.size(); ++i)
+				{
+					VectorEnemies[i]->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+				}
+			}
+			if (!VectorPowerups.empty())
+			{
+				for (int i = 0; i < VectorPowerups.size(); ++i)
+				{
+					VectorPowerups[i]->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+				}
+			}
+
+			player->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+
+			if (playerWin)
+			{
+				win->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+			}
+			if (playerLose)
+			{
+				lose->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+			}
+		}
+		else if (rendernumber == 2)
+		{
+			gameover->render(camera->getViewMatrix(), camera->getProjectionMatrix());
+		}
+	}
+
+	void update(GLfloat _deltaTime)
+	{
+		// enemies/camera wont move if in mainmenu or gameover screens
+		if (rendernumber == 0)
+		{
+			_deltaTime = 0;
 		}
 		else if (rendernumber == 1)
 		{
 
 		}
+		else if (rendernumber == 2)
+		{
+			camera->setEye(-1.0f, -6.0f, 4.0f);
+			camera->setCenter(0.0f, 0.5f, -1.0f);
+			camera->setUp(0.0f, 0.0f, 1.0f);
+			_deltaTime = 0;
+		}
+		if (lives == 0)
+		{
+			// Game over screen
+			rendernumber = 2;
+
+			// R to restart
+			if (keyState[(unsigned char)'r'] == BUTTON_DOWN)
+			{
+				isDeleted = true;
+			}
+			for (int i = 0; i < VectorBullets.size(); ++i)
+			{
+				for (int j = 0; j < VectorEnemies.size(); ++j)
+				{
+					std::swap(VectorBullets[i], VectorBullets.back());
+					VectorBullets.pop_back();
+					std::swap(VectorEnemies[j], VectorEnemies.back());
+					VectorEnemies.pop_back();
+				}
+			}
+
+		}
+		// If all enemies are dead
+		else if (VectorEnemies.size() == 0)
+		{
+			// When no enemies are on the screen, delete all bullets
+			for (int i = 0; i < VectorBullets.size(); ++i)
+			{
+				std::swap(VectorBullets[i], VectorBullets.back());
+				VectorBullets.pop_back();
+			}
+
+			level += 1;
+			std::cout << "Level: " << level << std::endl << std::endl << std::endl;
+
+			player->getPlayer()->setNewPosition(glm::vec3(-0.5f, -0.5f, 0.25f));
+
+			numOfEnemies += 1;
+			playerSpeed = 0.0275f;
+
+			for (int i = 0; i < VectorPowerups.size(); ++i)
+			{
+				VectorPowerups.pop_back();
+			}
+
+			VectorPowerups.push_back(new cPowerup());
+			VectorPowerups.back()->init();
+
+			for (int i = 0; i < numOfEnemies; ++i)
+			{
+				VectorEnemies.push_back(new cEnemies(0.015f));
+				VectorEnemies[i]->init();
+			}
+		}
+		for (int i = 0; i < VectorBullets.size(); ++i)
+		{
+			VectorBullets[i]->update(_deltaTime);
+		}
+		for (int i = 0; i < VectorEnemies.size(); ++i)
+		{
+			VectorEnemies[i]->update(_deltaTime, player->getPlayer()->getNewPositionX(), player->getPlayer()->getNewPositionY());
+		}
+		if (keyState[(unsigned char)'g'] == BUTTON_DOWN && rendernumber == 0)
+		{
+			rendernumber = 1;
+		}
+		if (keyState[(unsigned char)'w'] == BUTTON_DOWN && keyState[(unsigned char)'a'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed);
+		}
+		else if (keyState[(unsigned char)'w'] == BUTTON_DOWN && keyState[(unsigned char)'d'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed);
+		}
+		else if (keyState[(unsigned char)'s'] == BUTTON_DOWN && keyState[(unsigned char)'a'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed);
+		}
+		else if (keyState[(unsigned char)'s'] == BUTTON_DOWN && keyState[(unsigned char)'d'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed);
+		}
+		else if (keyState[(unsigned char)'w'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed + 0.0125);
+		}
+		else if (keyState[(unsigned char)'a'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed + 0.0125);
+		}
+		else if (keyState[(unsigned char)'s'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed + 0.0125);
+		}
+		else if (keyState[(unsigned char)'d'] == BUTTON_DOWN && playerAlive)
+		{
+			player->setSpeed(playerSpeed + 0.0125);
+		}
+		// R to restart
+		if (keyState[(unsigned char)'r'] == BUTTON_DOWN)
+		{
+			isDeleted = true;
+		}
+		// collisions
+		if (keyState[(unsigned char)'w'] == BUTTON_DOWN && playerAlive)
+		{
+			if (player->getPlayer()->getNewPositionY() > 3.6f)
+			{
+
+			}
+			else
+			{
+				player->getPlayer()->setNewPositionY(player->getPlayer()->getNewPositionY() + player->getSpeed() * _deltaTime);
+			}
+		}
+
+		if (keyState[(unsigned char)'a'] == BUTTON_DOWN && playerAlive)
+		{
+			if (player->getPlayer()->getNewPositionX() < -4.35f)
+			{
+
+			}
+			else
+			{
+				player->getPlayer()->setNewPositionX(player->getPlayer()->getNewPositionX() - player->getSpeed() * _deltaTime);
+			}
+		}
+
+		if (keyState[(unsigned char)'s'] == BUTTON_DOWN && playerAlive)
+		{
+			if (player->getPlayer()->getNewPositionY() < -3.45f)
+			{
+
+			}
+			else
+			{
+				player->getPlayer()->setNewPositionY(player->getPlayer()->getNewPositionY() - player->getSpeed() * _deltaTime);
+			}
+		}
+
+		if (keyState[(unsigned char)'d'] == BUTTON_DOWN && playerAlive)
+		{
+			if (player->getPlayer()->getNewPositionX() > 2.3f)
+			{
+
+			}
+			else
+			{
+				player->getPlayer()->setNewPositionX(player->getPlayer()->getNewPositionX() + player->getSpeed() * _deltaTime);
+			}
+		}
+		// Press to shoot
+		if (keyState[(unsigned char)'i'] == BUTTON_DOWN && playerAlive && cooldown < 0.0f)
+		{
+			cooldown = 25.0f;
+			VectorBullets.push_back(new cBullet(0, 1, player->getPlayer()->getNewPositionX(), player->getPlayer()->getNewPositionY()));
+			VectorBullets.back()->init();
+		}
+		if (keyState[(unsigned char)'k'] == BUTTON_DOWN && playerAlive && cooldown < 0.0f)
+		{
+			cooldown = 25.0f;
+			VectorBullets.push_back(new cBullet(0, -1, player->getPlayer()->getNewPositionX(), player->getPlayer()->getNewPositionY()));
+			VectorBullets.back()->init();
+		}
+		if (keyState[(unsigned char)'l'] == BUTTON_DOWN && playerAlive && cooldown < 0.0f)
+		{
+			cooldown = 25.0f;
+			VectorBullets.push_back(new cBullet(1, 0, player->getPlayer()->getNewPositionX(), player->getPlayer()->getNewPositionY()));
+			VectorBullets.back()->init();
+		}
+		if (keyState[(unsigned char)'j'] == BUTTON_DOWN && playerAlive && cooldown < 0.0f)
+		{
+			cooldown = 25.0f;
+			VectorBullets.push_back(new cBullet(-1, 0, player->getPlayer()->getNewPositionX(), player->getPlayer()->getNewPositionY()));
+			VectorBullets.back()->init();
+		}
+		cooldown -= _deltaTime;
+		// Bullets deleting on collision with wall
+		for (int i = 0; i < VectorBullets.size(); ++i)
+		{
+			if (VectorBullets[i]->getBullet()->getNewPositionX() > 5.0f || VectorBullets[i]->getBullet()->getNewPositionX() < -5.0f || VectorBullets[i]->getBullet()->getNewPositionY() > 5.0f || VectorBullets[i]->getBullet()->getNewPositionY() < -5.0f)
+			{
+				std::swap(VectorBullets[i], VectorBullets.back());
+				VectorBullets.pop_back();
+			}
+		}
+		// Bullet Enemy Collision
+		for (int i = 0; i < VectorBullets.size(); ++i)
+		{
+			for (int j = 0; j < VectorEnemies.size(); ++j)
+			{
+				if (((VectorBullets[i]->getBullet()->getNewPositionX() <= (VectorEnemies[j]->getEnemy()->getNewPositionX() + 0.35f)) && (VectorBullets[i]->getBullet()->getNewPositionX() >= (VectorEnemies[j]->getEnemy()->getNewPositionX() - 0.35f))) &&
+					((VectorBullets[i]->getBullet()->getNewPositionY() <= (VectorEnemies[j]->getEnemy()->getNewPositionY() + 0.35f)) && (VectorBullets[i]->getBullet()->getNewPositionY() >= (VectorEnemies[j]->getEnemy()->getNewPositionY() - 0.35f))))
+				{
+					std::swap(VectorBullets[i], VectorBullets.back());
+					VectorBullets.pop_back();
+					std::swap(VectorEnemies[j], VectorEnemies.back());
+					VectorEnemies.pop_back();
+					score += 30;
+					std::cout << "Score: " << score << std::endl;
+					break;
+				}
+			}
+		}
+		// Player Powerup Collision
+		for (int i = 0; i < VectorPowerups.size(); ++i)
+		{
+			if (player->getPlayer()->getNewPositionX() <= (VectorPowerups[i]->getPowerup()->getNewPositionX() + 0.35f) && player->getPlayer()->getNewPositionX() >= (VectorPowerups[i]->getPowerup()->getNewPositionX() - 0.35f) &&
+				player->getPlayer()->getNewPositionY() <= (VectorPowerups[i]->getPowerup()->getNewPositionY() + 0.35f) && player->getPlayer()->getNewPositionY() >= (VectorPowerups[i]->getPowerup()->getNewPositionY() - 0.35f))
+			{
+				std::cout << "WASD To move the character" << std::endl;
+				std::cout << "jilk to make the player shoot" << std::endl;
+				std::cout << "The player box is blue" << std::endl;
+				std::cout << "Red blocks are enemies, pink are powerups" << std::endl;
+				std::cout << "Walk over the powerup to pick it up, it will grant a small speed boost" << std::endl;
+
+				playerSpeed = 0.05;
+				std::swap(VectorPowerups[i], VectorPowerups.back());
+				VectorPowerups.pop_back();
+			}
+		}
+
+
+		// Enemy Player Collision
+		for (int i = 0; i < VectorEnemies.size(); ++i)
+		{
+			if (player->getPlayer()->getNewPositionX() <= (VectorEnemies[i]->getEnemy()->getNewPositionX() + 0.35f) && player->getPlayer()->getNewPositionX() >= (VectorEnemies[i]->getEnemy()->getNewPositionX() - 0.35f) &&
+				player->getPlayer()->getNewPositionY() <= (VectorEnemies[i]->getEnemy()->getNewPositionY() + 0.35f) && player->getPlayer()->getNewPositionY() >= (VectorEnemies[i]->getEnemy()->getNewPositionY() - 0.35f))
+			{
+				lives -= 1;
+				std::cout << "You have been hit by an enemy! Lives: " << lives << std::endl;
+				std::swap(VectorEnemies[i], VectorEnemies.back());
+				VectorEnemies.pop_back();
+			}
+		}
+
+		if (player->getPlayer()->getNewPositionX() >= -2.5f && player->getPlayer()->getNewPositionX() <= 0.5f)
+		{
+			camera->setCenterX(player->getPlayer()->getNewPositionX());
+		}
+
+		if (player->getPlayer()->getNewPositionY() > -1.0f && player->getPlayer()->getNewPositionY() <= 1.0f)
+		{
+			camera->setCenterY(player->getPlayer()->getNewPositionY());
+		}
+		camera->update();
 	}
+
+	bool getIsDeleted()
+	{
+		return isDeleted;
+	}
+
+	void keyboard(unsigned char key)
+	{
+		keyState[key] = BUTTON_DOWN;
+	}
+
+	void keyboard_up(unsigned char key) 
+	{
+		keyState[key] = BUTTON_UP;
+	}
+
+private:
+	
+	bool playerAlive;
+	bool playerWin;
+	bool playerLose;
+	bool enemy3MoveUp;
+	bool isDeleted;
+	int numOfEnemies;
+	float cooldown;
+	int score;
+	int lives;
+	int level;
+	int rendernumber;
+	float playerSpeed;
+
+	cEnemies* enemy;
+	cPlayer* player;
+	cSprite* gameover;
+	cSprite* mainmenu;
+	cSprite* background;
+	cSprite* win;
+	cSprite* lose;
+	cCamera* camera;
+	std::vector<cPowerup*> VectorPowerups;
+	std::vector<cBullet*> VectorBullets;
+	std::vector<cEnemies*> VectorEnemies;
+
+
 };
